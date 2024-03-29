5019382399f1ba2344e916988ecf92a0a1eb2f6e
Carolina Simón <carolina.simon.guerrero@gmail.com>
2024-03-29 19:53:26 +0100

    Update database name

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from views import register_views
 app = Flask(__name__)
 
 # Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fandango.db' # URI configuration (sqlite and fandango.db)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fandango-core.db' # URI configuration (sqlite and fandango.db)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Associate db to flask app
